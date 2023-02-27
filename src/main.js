8679e089f2dad7e16ac2a5d1b12dc5d2edf66bdd
falana sheriff <falanasheriffdeen@gmail.com>
2023-02-27 14:12:09 +0100

    derer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Flutterwave from 'flutterwave-vue3'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(Flutterwave).mount('#app')
+createApp(App).use(Flutterwave).use(router).mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ea voluptas odio quo sequi veritatis ipsum similique! Dignissimos, ratione. Blanditiis dolor similique temporibus, officia reiciendis quia sit reprehenderit sed saepe?</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit veritatis corrupti sint incidunt possimus beatae quibusdam, optio blanditiis reprehenderit facere cum, officiis quas quasi ipsam ea, iure </p>
   </div>
 </template>
